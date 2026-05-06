71b4dfa8d7e5cc6b8618b307fd137bcbf68c6622
LeQuangMien10 <lequangmien10@gmail.com>
2026-05-07 02:08:11 +0700

    Turn off augmentations

--- data/transforms.py
+++ data/transforms.py
@@ -3,11 +3,13 @@
 import albumentations as A
 
 def get_train_transform():
-    return A.Compose([
-        A.GaussianBlur(p=0.3),
-        A.RandomBrightnessContrast(p=0.3),
-        A.Rotate(limit=3, p=0.3),
-    ])
+    # return A.Compose([
+    #     A.GaussianBlur(p=0.3),
+    #     A.RandomBrightnessContrast(p=0.3),
+    #     A.Rotate(limit=3, p=0.3),
+    # ])
+
+    return A.Compose([])
 
 def get_val_transform():
     return A.Compose([])
